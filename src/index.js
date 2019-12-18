e4fdade100dd1b47f4554426de814c104e1b3439
Siarhei Roukach <siarhei.roukach@itechart-group.com>
2019-12-18 11:30:56 +0300

    delete route from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import configureStore from "./store/configureStore";
@@ -10,7 +10,7 @@ import "./index.scss";
 ReactDOM.render(
   <Provider store={configureStore}>
     <Router>
-      <Route path="/" component={App} />
+      <App />
     </Router>
   </Provider>,
   document.getElementById("root")
